5f550735d50952014ce453dba55401c6336cd9bd
MartinJindu <martinchijindu@gmail.com>
2025-05-19 02:31:08 +0100

    login logic

--- src/models/User.js
+++ src/models/User.js
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-  },
-  profileImage: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 // hash password before saving to db
 userSchema.pre("save", async function (next) {
@@ -33,6 +36,12 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+// compare password func
+// we add this method to userSchema model
+userSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
